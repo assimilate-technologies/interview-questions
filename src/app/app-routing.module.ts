fab89a83ba78b9bec817cb279ac46dfcf2712409
Mahadev <mahadev.punekar@sazingadigital.com>
2022-08-30 12:37:34 +0530

    update questions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'angular', loadChildren: () => import('./angular/angular.module').then(m => m.AngularModule) }, { path: 'reactjs', loadChildren: () => import('./reactjs/reactjs.module').then(m => m.ReactjsModule) }, { path: 'javascript', loadChildren: () => import('./javascript/javascript.module').then(m => m.JavascriptModule) }];
+const routes: Routes = [
+  {
+    path: 'angular',
+    loadChildren: () => import('./angular/angular.module').then(m => m.AngularModule)
+  },
+  {
+    path: 'reactjs',
+    loadChildren: () => import('./reactjs/reactjs.module').then(m => m.ReactjsModule)
+  },
+  {
+    path: 'javascript',
+    loadChildren: () => import('./javascript/javascript.module').then(m => m.JavascriptModule)
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
